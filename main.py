47f3da0df12934015193cb8f369c31c8ae9dadc7
Rishith <rishith.prasanna@rafay.co>
2025-02-28 07:03:11 +0000

    Added Python script

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("Hello Jenkins!")
-print("Welcome to Rafay!")
+import random
+
+def generate_random_number():
+    num = random.randint(1, 100)
+    print(f"Generated Random Number: {num}")
+
+if __name__ == "__main__":
+    generate_random_number()
+
