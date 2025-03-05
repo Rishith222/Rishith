3d902e965800d81bac73c47fdc2b87ef8e48e1a0
Rishith222 <rishith.prasanna@rafay.co>
2025-03-05 15:31:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,89 +1,26 @@
-<<<<<<< HEAD
 terraform {
   required_providers {
-<<<<<<< HEAD
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-=======
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
->>>>>>> 2051675 (Update main.tf)
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
     }
   }
 }
 
-<<<<<<< HEAD
-provider "docker" {
-  host = "unix:///var/run/docker.sock"  # Ensure Jenkins has access to this
+provider "github" {
+  token = var.github_token  # Use a personal access token (PAT) with repo access
 }
 
-resource "docker_image" "jenkins_agent" {
-  name = "my-jenkins-agent:latest"
-  keep_locally = true
+variable "github_token" {}
+
+variable "repo_name" {
+  default = "Rishith222"
 }
 
-resource "docker_container" "jenkins_docker_agent" {
-  name  = "jenkins-docker-agent"
-  image = docker_image.jenkins_agent.name
-
-  # Attach Docker socket for Jenkins agent to run containers
-  volumes {
-    host_path      = "/var/run/docker.sock"
-    container_path = "/var/run/docker.sock"
-  }
-
-  # Set Jenkins workspace
-  volumes {
-    host_path      = "/var/lib/jenkins/workspace"
-    container_path = "/var/lib/jenkins/workspace"
-  }
-
-  # Expose ports if needed
-  ports {
-    internal = 8080
-    external = 8081
-  }
-
-  restart = "always"
-=======
-resource "null_resource" "local_node" {
-=======
-provider "local" {}
-
-resource "local_file" "example_file" {
-  filename = "${path.module}/example-folder/hello.txt"
-  content  = "This is created by Terraform inside a Docker agent!"
+resource "github_repository_file" "example_file" {
+  repository          = var.repo_name
+  branch             = "master"
+  file               = "my_folder/example.txt"
+  content            = "This is a sample file created by Terraform."
+  commit_message     = "Added example.txt via Terraform"
 }
-
-resource "null_resource" "create_folder" {
->>>>>>> 76e8b33 (Update main.tf)
-  provisioner "local-exec" {
-    command = "mkdir -p ${path.module}/example-folder"
-  }
-}
-<<<<<<< HEAD
-
-provider "null" {}
-
-resource "null_resource" "create_folder_and_file" {
-  provisioner "local-exec" {
-    command = <<EOT
-    mkdir -p /tmp/myfolder
-    echo "Hello, Terraform!" > /tmp/myfolder/myfile.txt
-    EOT
-  }
-}
-
-
-variable "env" {
-  default = "dev"
-}
-
-variable "region" {
-  default = "local"
->>>>>>> 2051675 (Update main.tf)
-}
-=======
->>>>>>> 76e8b33 (Update main.tf)
